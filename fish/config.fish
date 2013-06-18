c24c35a739202e235ef38d4f5df90083288ee3a6
Jeff Larson <thejefflarson@gmail.com>
2013-06-18 14:40:08 -0400

    tweak icons

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,14 @@
 set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch yellow
-set __fish_git_prompt_char_dirtystate '•'
-set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_color_branch green
+set __fish_git_prompt_color_dirtystate red
+set __fish_git_prompt_color_staged yellow
+set __fish_git_prompt_color_upstream green
+set __fish_git_prompt_char_dirtystate '• '
+set __fish_git_prompt_char_stagedstate '→ '
 set __fish_git_prompt_char_upstream_ahead '↑'
 set __fish_git_prompt_char_upstream_behind '↓'
+set __fish_git_prompt_char_upstream_equal '='
 set -x fish_greeting
 
 set -x PATH $HOME/dev/git-data/bin $HOME/bin /usr/local/bin /usr/local/checker $PATH /Applications/git-annex.app/Contents/MacOS
@@ -17,4 +19,5 @@ set -x EDITOR vim
 
 . ~/.config/secrets.fish
 . /usr/local/share/chruby/chruby.fish
+. /usr/local/share/chruby/auto.fish
 chruby ruby-2.0
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,17 +1,17 @@
 function fish_prompt --description 'Write out the prompt'
-	
+
 	set -l last_status $status
 
 	if not set -q __fish_prompt_normal
 		set -g __fish_prompt_normal (set_color normal)
 	end
-	
+
 	set -l delim ' $ '
 
 	set -l prompt_status
 	if test $last_status -ne 0
 		if not set -q __fish_prompt_status
-			set -g __fish_prompt_status (set_color $fish_color_status)
+			set -g __fish_prompt_status (set_color red)
 		end
 		set prompt_status "$__fish_prompt_status [$last_status]$__fish_prompt_normal"
 	end
