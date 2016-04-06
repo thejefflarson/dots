061fa885e69778314afe0853b4b266ab498f307e
Jeff Larson <thejefflarson@gmail.com>
2016-04-06 16:09:20 -0400

    waypoint on email

--- emacs/.emacs.d/private/jeff-email/packages.el
+++ emacs/.emacs.d/private/jeff-email/packages.el
@@ -27,8 +27,13 @@
   (setq mu4e-get-mail-command "offlineimap -u quiet; true")
   (setq mu4e-maildir "~/.mail/gmail")
   (setq mu4e-mu-home "~/.mail/mu-gmail")
+  (setq mu4e-compose-dont-reply-to-self t)
   (add-hook 'mu4e-compose-mode-hook 'epa-mail-mode)
-  (add-hook 'mu4e-view-mode-hook 'epa-mail-mode))
+  (add-hook 'mu4e-view-mode-hook 'epa-mail-mode)
+  (setq message-send-mail-function 'message-send-mail-with-sendmail)
+  (setq sendmail-program "/usr/local/bin/msmtp")
+  (setq message-sendmail-extra-arguments '("--read-envelope-from"))
+  (setq message-sendmail-f-is-evil 't))
 
 (defun jeff-email/init-mu4e-context ()
   "Define different email contexts"
@@ -40,7 +45,9 @@
                :enter-func (lambda ()
                              (mu4e-message "entering gmail"))
                :match-func (lambda (msg)
-                             (if msg (string-equal "home" (mu4e-context-name mu4e~context-current)) t))
+                             (when msg
+                               (mu4e-message-contact-field-matches msg :to "thejefflarson@gmail.com")))
+               :leave-func (lambda () (mu4e-clear-caches))
                :vars '((mail-reply-to . "thejefflarson@gmail.com")
                        (user-mail-address . "thejefflarson@gmail.com")
                        (user-full-name . "Jeff Larson")
@@ -49,9 +56,38 @@
                        (mu4e-trash-folder . "/[Gmail].Trash")
                        (mu4e-maildir ."~/.mail/gmail")
                        (mu4e-mu-home . "~/.mail/mu-gmail")
-                       (mu4e-refile-folder . "/[Gmail].All Mail"))))
+                       (mu4e-refile-folder . "/[Gmail].All Mail")))
+             ,(make-mu4e-context
+               :name "work"
+               :enter-func (lambda ()
+                             (mu4e-message "entering work"))
+               :match-func (lambda (msg)
+                             (when msg
+                               (mu4e-message-contact-field-matches msg :to "jeff.larson@propublica.org"))
+               :leave-func (lambda () (mu4e-clear-caches))
+               :vars '((mail-reply-to . "jeff.larson@propublica.org")
+                       (user-mail-address . "jeff.larson@propublica.org")
+                       (user-full-name . "Jeff Larson")
+                       (mu4e-maildir ."~/.mail/work")
+                       (mu4e-mu-home . "~/.mail/mu-work")
+                       (mu4e-refile-folder . "/archive"))))
+             ,(make-mu4e-context
+               :name "riseup"
+               :enter-func (lambda ()
+                             (mu4e-message "entering riseup"))
+               :match-func (lambda (msg)
+                             (when msg
+                               (mu4e-message-contact-field-matches msg :to "thejefflarson@riseup.net"))
+               :leave-func (lambda () (mu4e-clear-caches))
+               :vars '((mail-reply-to . "thejefflarson@riseup.net")
+                       (user-mail-address . "thejefflarson@riseup.net")
+                       (user-full-name . "Jeff Larson")
+                       (mu4e-maildir ."~/.mail/riseup")
+                       (mu4e-mu-home . "~/.mail/mu-riseup"))))
+             )
           )
-    ))
+    )
+  )
 
 (defun jeff-email/init-mu4e-contrib ()
   (use-package mu4e-contrib
--- emacs/.spacemacs
+++ emacs/.spacemacs
@@ -120,7 +120,7 @@ values."
                                :size 13
                                :weight normal
                                :width normal
-                               :powerline-scale 1.1)
+                               :powerline-scale 1.0)
    ;; The leader key
    dotspacemacs-leader-key "SPC"
    ;; The leader key accessible in `emacs state' and `insert state'
@@ -263,7 +263,7 @@ you should place you code here."
       (progn
         (setq insert-directory-program "/usr/local/bin/gls")
         (setq dired-listing-switches "-aBhl --group-directories-first")))
-
+  (spaceline-toggle-hud-off)
   (setq powerline-default-separator nil)
   )
 
