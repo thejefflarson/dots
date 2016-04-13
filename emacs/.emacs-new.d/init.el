30f848e298f9d36cab3d43468ddde114d1d05ebe
Jeff Larson <thejefflarson@gmail.com>
2016-04-12 22:18:39 -0400

    More fun configuration. this is pretty nice.

--- emacs/.emacs-new.d/init.el
+++ emacs/.emacs-new.d/init.el
@@ -1,16 +1,29 @@
+;;; jeffs-init --- various bits and boops to make emacs a nicer place
+
+;;; Commentary:
+;; Too many late nights copying and pasting from the internet,
+;; but hey I'm proud of it.
+;;; Code:
 
 ;; Configuration
+(setq user-full-name "Jeff Larson"
+      user-mail-address "thejefflarson@gmail.com")
+
 (setq load-prefer-newer t)
 
 ;; Allow the -l flag to find the right emacs directory
 (defconst user-emacs-directory
   (file-name-directory (or load-file-name (buffer-file-name)))
-  "Current emacs.d directory")
+  "Current emacs.d directory.")
 
 (defconst user-cache-directory
   (file-name-as-directory
    (concat user-emacs-directory ".cache"))
-  "Directory for temporary files.")
+   "Directory for temporary files.")
+
+(unless (and (file-exists-p user-cache-directory)
+	     (file-accessible-directory-p user-cache-directory))
+  (mkdir user-cache-directory))
 
 ;; OS X specific configuration
 (when (eq system-type 'darwin)
@@ -30,8 +43,9 @@
 ;; Don't muddy init.el with custom stuff
 (setq-default custom-file
               (concat user-cache-directory "custom.el"))
-(when (file-exists-p custom-file)
-  (load-file custom-file))
+(if (file-exists-p custom-file)
+    (load-file custom-file)
+  (write-region "" nil custom-file t))
 
 ;; Turn off a bunch of useless stuff
 (when (featurep 'menu-bar)
@@ -55,9 +69,8 @@
 
 ;; Builtin configuration
 (eval-after-load 'srecode
-  '(progn
-     (setq srecode-map-save-file
-           (concat user-cache-directory "srecode-map.el"))))
+  '(setq srecode-map-save-file
+	 (concat user-cache-directory "srecode-map.el")))
 
 
 ;; Packages
@@ -68,16 +81,26 @@
 (cask-initialize)
 (require 'pallet)
 (pallet-mode t)
-
 (require 'req-package)
+
+(req-package diminish)
+
+(req-package smooth-scrolling
+  :config
+  (smooth-scrolling t))
+
+(req-package rainbow-delimiters
+  :init
+  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
+
 (req-package swiper
-             :init
-             (setq ivy-use-virtual-buffers t)
-             (setq ivy-height 10)
-             (setq ivy-count-format "(%d/%d) ")
-             :config
-             (ivy-mode 1)
-             (global-set-key (kbd "C-s") 'swiper))
+  :init
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-height 10)
+  (setq ivy-count-format "(%d/%d) ")
+  :config
+  (ivy-mode 1)
+  (global-set-key (kbd "C-s") 'swiper))
 
 (req-package counsel
   :require swiper
@@ -95,41 +118,50 @@
   (global-set-key (kbd "C-x l") 'counsel-locate)
   (global-set-key (kbd "C-S-o") 'counsel-rhythmbox))
 
+
 (req-package undo-tree
-             :config
-             (global-undo-tree-mode))
+  :config
+  (global-undo-tree-mode))
 
 (req-package flycheck
-             :config
-             (global-flycheck-mode))
+  :config
+  (global-flycheck-mode))
 
 (req-package projectile
-             :requires swiper
-             :config
-             (setq projectile-completion-system 'ivy))
+  :requires swiper
+  :config
+  (setq projectile-completion-system 'ivy))
 
 (req-package magit
-             :requires swiper
-             :config
-             (setq magit-completing-read-function 'ivy-completing-read))
+  :requires swiper
+  :config
+  (setq magit-completing-read-function 'ivy-completing-read))
 
 (req-package which-key
-             :config
-             (which-key-mode))
+  :config
+  (which-key-mode))
 
 (req-package kurecolor)
 (req-package rainbow-mode)
 
 (req-package diff-hl
-             :require magit
-             :init
-             (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
-             :config
-             (global-diff-hl-mode 1))
+  :require magit
+  :init
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
+  :config
+  (global-diff-hl-mode 1))
 
 (req-package page-break-lines
   :config
-  (turn-on-page-break-lines-mode))
+  (global-page-break-lines-mode))
+
+(req-package smartparens)
+(req-package smartparens-config
+  :require smartparens)
+
+(req-package winner
+  :config
+  (winner-mode t))
 
 
 ;; Programming modes
@@ -151,145 +183,129 @@
              (add-to-list 'completion-ignored-extensions ".rbc"))
 
 (req-package projectile-rails
-             :require projectile
-             :init
-             (add-hook 'projectile-mode-hook 'projectile-rails-on))
-
-(req-package rust-mode
-             :mode "\\.rs\\'")
-
-(req-package racer)
-
-(req-package js2-mode
-             :mode "\\.js\\'")
-
-(req-package jsx-mode
-             :mode "\\.jsx\\'")
-
-(req-package cc-mode
-             :mode (("\\.cpp\\'" . c++-mode)
-                    ("\\.hpp\\'" . c++-mode)
-                    ("\\.h\\'" . c++-mode))
-             :config
-             (c-add-style "cc-style" cc-style)
-             (setq-default c-basic-offset 4)
-             (setq-default c-default-style "cc-style"))
+  :mode (("\\.cpp\\'" . c++-mode)
+         ("\\.hpp\\'" . c++-mode)
+         ("\\.h\\'" . c++-mode))
+  :config
+  (c-add-style "cc-style" cc-style)
+  (setq-default c-basic-offset 4)
+  (setq-default c-default-style "cc-style"))
 
 (req-package gdb-mi
-             :require cc-mode
-             :config
-             (setq gdb-many-windows t)
-             (setq gdb-show-main t))
+  :require cc-mode
+  :config
+  (setq gdb-many-windows t)
+  (setq gdb-show-main t))
 
 (req-package css-mode
-             :require kurecolor rainbow-mode
-             :mode "\\.css\\'"
-             :config
-             (rainbow-mode))
+  :require kurecolor rainbow-mode
+  :mode "\\.css\\'"
+  :config
+  (rainbow-mode))
 
 
 ;; Mu4e
 (req-package mu4e
-             :init
-             (setq mu4e-update-interval (* 60 5))
-             (setq mu4e-get-mail-command "offlineimap -u quiet; true")
-             (setq mu4e-compose-dont-reply-to-self t)
-             (setq mu4e-user-mail-address-list '("thejefflarson@gmail.com",
-                                                 "jeff.larson@propublica.org",
-                                                 "thejefflarson@riseup.net"))
-             (setq mu4e-context-policy 'pick-first)
-             (setq mu4e-maildir "~/.mail")
-             (setq mu4e-headers-skip-duplicates t)
-             (setq mu4e-headers-visible-lines 20)
-             (setq mu4e-view-show-addresses 'long)
-             (setq mu4e-compose-in-new-frame t)
-             (setq message-send-mail-function 'message-send-mail-with-sendmail)
-             (setq sendmail-program "/usr/local/bin/msmtp")
-             (setq message-sendmail-extra-arguments '("--read-envelope-from"))
-             (setq message-sendmail-f-is-evil 't)
-             :config
-             (add-hook 'mu4e-compose-mode-hook 'epa-mail-mode)
-             (add-hook 'mu4e-view-mode-hook 'epa-mail-mode)
-             (add-hook 'mu4e-view-mode-hook 'visual-line-mode))
+  :init
+  (setq mu4e-update-interval (* 60 5))
+  (setq mu4e-get-mail-command "offlineimap -u quiet; true")
+  (setq mu4e-compose-dont-reply-to-self t)
+  (setq mu4e-user-mail-address-list '("thejefflarson@gmail.com",
+                                      "jeff.larson@propublica.org",
+                                      "thejefflarson@riseup.net"))
+  (setq mu4e-context-policy 'pick-first)
+  (setq mu4e-maildir "~/.mail")
+  (setq mu4e-headers-skip-duplicates t)
+  (setq mu4e-headers-visible-lines 20)
+  (setq mu4e-view-show-addresses 'long)
+  (setq mu4e-compose-in-new-frame t)
+  (setq message-send-mail-function 'message-send-mail-with-sendmail)
+  (setq sendmail-program "/usr/local/bin/msmtp")
+  (setq message-sendmail-extra-arguments '("--read-envelope-from"))
+  (setq message-sendmail-f-is-evil 't)
+  :config
+  (add-hook 'mu4e-compose-mode-hook 'epa-mail-mode)
+  (add-hook 'mu4e-view-mode-hook 'epa-mail-mode)
+  (add-hook 'mu4e-view-mode-hook 'visual-line-mode))
 
-(req-package mu4e-contrib
-             :init (setq mu4e-html2text-command 'mu4e-shr2text))
+(req-package mu4e-contrib :init
+  (setq mu4e-html2text-command 'mu4e-shr2text))
 
 (req-package mu4e-maildirs-extension
-             :require mu4e
-             :init (setq mu4e-maildirs-extension-fake-maildir-separator "\\.")
-             :config (mu4e-maildirs-extension))
+  :require mu4e
+  :init (setq mu4e-maildirs-extension-fake-maildir-separator "\\.")
+  :config (mu4e-maildirs-extension))
 
 (req-package mu4e-alert
-             :init
-             (setq mu4e-alert-interesting-mail-query 
-                   (concat 
-                    "flag:unread date:today..now \("
-                    "maildir:/work/INBOX"
-                    " OR maildir:/gmail/INBOX"
-                    " OR maildir:/gmail/[Gmail].All\ Mail"
-                    " OR maildir:/gmail/[Gmail].Important"
-                    " OR maildir:/riseup/INBOX\)"))
-             :config
-             (mu4e-alert-set-default-style (if (eq system-type 'darwin)
-                                               'notifier 'notifications))
-             (mu4e-alert-enable-notifications)
-             (mu4e-alert-enable-mode-line-display))
+  :init
+  (setq mu4e-alert-interesting-mail-query 
+        (concat 
+         "flag:unread date:today..now \("
+         "maildir:/work/INBOX"
+         " OR maildir:/gmail/INBOX"
+         " OR maildir:/gmail/[Gmail].All\ Mail"
+         " OR maildir:/gmail/[Gmail].Important"
+         " OR maildir:/riseup/INBOX\)"))
+  :config
+  (mu4e-alert-set-default-style (if (eq system-type 'darwin)
+                                    'notifier 'notifications))
+  (mu4e-alert-enable-notifications)
+  (mu4e-alert-enable-mode-line-display))
 
 (req-package epg-config
-             :init
-             (setq mml2015-use 'epg
-                   mml2015-encrypt-to-self t
-                   mml2015-sign-with-sender t))
+  :init
+  (setq mml2015-use 'epg
+        mml2015-encrypt-to-self t
+        mml2015-sign-with-sender t))
 
 (req-package mu4e-context
-             :config
-             (setq mu4e-contexts
-                   `( ,(make-mu4e-context
-                        :name "gmail"
-                        :enter-func (lambda ()
-                                      (mu4e-message "entering gmail"))
-                        :match-func (lambda (msg)
-                                      (when msg
-                                        (string-match "gmail" (mu4e-message-field msg :maildir))))
-                        :vars '((mail-reply-to . "thejefflarson@gmail.com")
-                                (user-mail-address . "thejefflarson@gmail.com")
-                                (user-full-name . "Jeff Larson")
-                                (mu4e-sent-messages-behavior . delete)
-                                (mu4e-drafts-folder . "/gmail/[Gmail].Drafts")
-                                (mu4e-sent-folder . "/gmail/[Gmail].Sent Mail")
-                                (mu4e-trash-folder . "/gmail/[Gmail].Trash")
-                                (mu4e-refile-folder . "/gmail/[Gmail].All Mail")))
-                      ,(make-mu4e-context
-                        :name "work"
-                        :enter-func (lambda ()
-                                      (mu4e-message "entering work"))
-                        :match-func (lambda (msg)
-                                      (when msg
-                                        (string-match "work" (mu4e-message-field msg :maildir))))
-                        :vars '((mail-reply-to . "jeff.larson@propublica.org")
-                                (user-mail-address . "jeff.larson@propublica.org")
-                                (user-full-name . "Jeff Larson")
-                                (mu4e-sent-messages-behavior . sent)
-                                (mu4e-drafts-folder . "/work/Drafts")
-                                (mu4e-sent-folder . "/work/Sent")
-                                (mu4e-trash-folder . "/work/Trash")
-                                (mu4e-refile-folder . "/work/archive")))
-                      ,(make-mu4e-context
-                        :name "riseup"
-                        :enter-func (lambda ()
-                                      (mu4e-message "entering riseup"))
-                        :match-func (lambda (msg)
-                                      (when msg
-                                        (string-match "riseup" (mu4e-message-field msg :maildir))))
-                        :vars '((mail-reply-to . "thejefflarson@riseup.net")
-                                (user-mail-address . "thejefflarson@riseup.net")
-                                (user-full-name . "Jeff Larson")
-                                (mu4e-sent-messages-behavior . sent)
-                                (mu4e-drafts-folder . "/riseup/Drafts")
-                                (mu4e-sent-folder . "/riseup/Sent")
-                                (mu4e-trash-folder . "/riseup/Trash")
-                                (mu4e-refile-folder . "/riseup/Archive"))))))
+  :config
+  (setq mu4e-contexts
+     `( ,(make-mu4e-context
+          :name "gmail"
+          :enter-func (lambda ()
+			(mu4e-message "entering gmail"))
+          :match-func (lambda (msg)
+                        (when msg
+                          (string-match "gmail" (mu4e-message-field msg :maildir))))
+          :vars '((mail-reply-to . "thejefflarson@gmail.com")
+                  (user-mail-address . "thejefflarson@gmail.com")
+                  (user-full-name . "Jeff Larson")
+                  (mu4e-sent-messages-behavior . delete)
+                  (mu4e-drafts-folder . "/gmail/[Gmail].Drafts")
+                  (mu4e-sent-folder . "/gmail/[Gmail].Sent Mail")
+                  (mu4e-trash-folder . "/gmail/[Gmail].Trash")
+                  (mu4e-refile-folder . "/gmail/[Gmail].All Mail")))
+        ,(make-mu4e-context
+          :name "work"
+          :enter-func (lambda ()
+                        (mu4e-message "entering work"))
+          :match-func (lambda (msg)
+                        (when msg
+                          (string-match "work" (mu4e-message-field msg :maildir))))
+          :vars '((mail-reply-to . "jeff.larson@propublica.org")
+                  (user-mail-address . "jeff.larson@propublica.org")
+                  (user-full-name . "Jeff Larson")
+                  (mu4e-sent-messages-behavior . sent)
+                  (mu4e-drafts-folder . "/work/Drafts")
+                  (mu4e-sent-folder . "/work/Sent")
+                  (mu4e-trash-folder . "/work/Trash")
+                  (mu4e-refile-folder . "/work/archive")))
+        ,(make-mu4e-context
+          :name "riseup"
+          :enter-func (lambda ()
+                        (mu4e-message "entering riseup"))
+          :match-func (lambda (msg)
+                        (when msg
+                          (string-match "riseup" (mu4e-message-field msg :maildir))))
+          :vars '((mail-reply-to . "thejefflarson@riseup.net")
+                  (user-mail-address . "thejefflarson@riseup.net")
+                  (user-full-name . "Jeff Larson")
+                  (mu4e-sent-messages-behavior . sent)
+                  (mu4e-drafts-folder . "/riseup/Drafts")
+                  (mu4e-sent-folder . "/riseup/Sent")
+                  (mu4e-trash-folder . "/riseup/Trash")
+                  (mu4e-refile-folder . "/riseup/Archive"))))))
 
 
 ;; Theme
@@ -299,11 +315,16 @@
   (mapc #'disable-theme custom-enabled-themes))
 
 (req-package color-theme-sanityinc-tomorrow
-             :config
-             (load-theme 'color-theme-sanityinc-tomorrow t))
+  :config
+  (color-theme-sanityinc-tomorrow-eighties))
 
-(when (eq system-type 'darwin)
-  (set-face-attribute 'default nil :family "Monaco")
-  (set-face-attribute 'default nil :height 120))
+(req-package faces
+  :config
+  (progn
+    (when (eq system-type 'darwin)
+      (set-face-attribute 'default nil :family "Monaco"))
+    (when (eq system-type 'gnu/linux)
+      (set-face-attribute 'default nil :family "Source Code Pro"))
+    (set-face-attribute 'default nil :height 120)))
 
 (req-package-finish)
