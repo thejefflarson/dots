a380567f0da04b2bb24c0024da63036bfdf9f9ff
Jeff Larson <thejefflarson@gmail.com>
2021-09-25 12:48:40 -0700

    be a person

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -611,7 +611,7 @@
   (verilog-case-indent-level 2)
   (verilog-indent-level-module 2)
   (verilog-indent-level-behavioral 2)
-  (verilog-indent-declaration 2)
+  (verilog-indent-level-declaration 2)
   (verilog-auto-newline nil)
   :config
   ;; Jesus autocomplete sucks in this mode, just because you can doesn't mean you should
