51cecca7406acfd92e9b4c083cf503222dba5240
Jeff Larson <thejefflarson@gmail.com>
2022-04-24 20:12:35 -0700

    early init

---
+++ emacs/.emacs.d/early-init.el
@@ -0,0 +1,7 @@
+;;; jeffs-early-init --- another collection of bloops for emacs
+;;; Commentary:
+;; This file exists to tweak the gui before we boot
+;;; Code:
+(add-to-list 'default-frame-alist '(undecorated . t))
+(provide 'early-init)
+;;; early-init.el ends here
